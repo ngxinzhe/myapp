2233e137e6e17504c03bc916078474342ce9bd59
ngxinzhe <xinzhe.ng@innoqode.com>
2021-11-17 15:09:49 +0800

    change to innoqode mail

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Yourrrr Vue.js App"/>
+    <HelloWorld msg="Welcome to Yourss Vue.js App"/>
   </div>
 </template>
 
